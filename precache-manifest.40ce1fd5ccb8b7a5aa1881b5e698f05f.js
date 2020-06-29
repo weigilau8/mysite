3167458a9705609e28107768a3dea5cb9f00f1bf
lui <weigilauron@gmail.com>
2020-06-30 00:40:48 +0800

    Updates

---
+++ precache-manifest.40ce1fd5ccb8b7a5aa1881b5e698f05f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e666500d2592c9de6caa19b8b4d4b6",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "78b970f946863129f29d",
+    "url": "/mysite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30c575e627ebae51e775",
+    "url": "/mysite/static/js/2.52cf36a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mysite/static/js/2.52cf36a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b970f946863129f29d",
+    "url": "/mysite/static/js/main.6866c93b.chunk.js"
+  },
+  {
+    "revision": "1d99fe0da908ca569518",
+    "url": "/mysite/static/js/runtime-main.1d89857a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mysite/static/media/logo.5d5d9eef.svg"
+  }
+]);
