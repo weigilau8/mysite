60e3efeaf504f33ef16cd75039b6318ae8c1fbd7
lui <weigilauron@gmail.com>
2020-06-30 00:54:52 +0800

    2nd

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.css';
+
+const Header = () => (
+    <div className="navbar">
+        <a href="#home" class="active">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+    </div>
+)
+
+export default Header;
